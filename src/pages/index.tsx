0eb9ac2dbe6c2abd79bbe84d307ec6f9bd3f3aee
thanhdat17ck1 <58363256+thanhdat17ck1@users.noreply.github.com>
2024-03-06 00:21:09 +0700

    commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React from 'react'
+import Head from 'next/head';
+
+const Page = () => {
+  
+  return (
+    <>
+      <Head>
+        <title>Trang Chủ - Crystal</title>
+      </Head>
+      <main>
+        hello world
+      </main>
+    </>
+  )
+}
+
+
+export default Page;
+
